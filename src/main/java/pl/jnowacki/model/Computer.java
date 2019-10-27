bc81182b41bb6e5caea8aca0cbe2ffac2d76ef39
Jedrzej Nowacki <contact@jnowacki.pl>
2019-10-27 15:22:07 +0100

    Initial commit

---
+++ src/main/java/pl/jnowacki/model/Computer.java
@@ -0,0 +1,48 @@
+package pl.jnowacki.model;
+
+public class Computer {
+    private Keyboard keyboard;
+    private Screen screen;
+    private Printer printer;
+
+    public Computer(Keyboard keyboard, Screen screen, Printer printer) {
+        this.keyboard = keyboard;
+        this.screen = screen;
+        this.printer = printer;
+    }
+
+    public void readValueFromKeyboardAndPrintToPrinterWithAllCaps(){
+        String inputFromKeyboard = keyboard.readValue();
+        printer.print(inputFromKeyboard.toUpperCase());
+    }
+
+    public void readValueFromKeyboardAndDisplayInputToBothPrinterAndScreen(){
+        String inputFromKeyboard = keyboard.readValue();
+        printer.print(inputFromKeyboard);
+        screen.showMessage(inputFromKeyboard);
+    }
+
+    public Keyboard getKeyboard() {
+        return keyboard;
+    }
+
+    public void setKeyboard(Keyboard keyboard) {
+        this.keyboard = keyboard;
+    }
+
+    public Screen getScreen() {
+        return screen;
+    }
+
+    public void setScreen(Screen screen) {
+        this.screen = screen;
+    }
+
+    public Printer getPrinter() {
+        return printer;
+    }
+
+    public void setPrinter(Printer printer) {
+        this.printer = printer;
+    }
+}
---
+++ src/test/java/pl/jnowacki/ComputerTest.java
@@ -0,0 +1,18 @@
+package pl.jnowacki;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import pl.jnowacki.model.Computer;
+import pl.jnowacki.model.Printer;
+
+import static org.mockito.Mockito.*;
+
+public class ComputerTest {
+
+    @Test
+    public void test() {
+    }
+}
